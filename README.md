23cae2e6423fc73ca0854407cfc1b4efd568a061
Sean <jb@JBs-MacBook-Pro.local>
2021-05-04 18:35:38 -0500

    added boilerplate code and packages

--- README.md
+++ README.md
@@ -1,7 +1,31 @@
-# React Template
+# SR React Template
 
 **Use this template to get up and running quickly on projects.**
 
-This is just like the webpack template. Use it, create the repo, connect it to your local and `npm i`.
+To run, install npm
 
-- [Steps to deploy](https://github.com/nss-nightclass-projects/REACT-Deployment-Netlify)
+``
+npm install
+``
+
+### Packages
+Included in this template are the following packages/frameworks/CDNs
+- Material UI Core
+
+``
+npm install @material-ui/core
+``
+- Material UI Icons
+
+``
+npm install @material-ui/icons
+``
+- Moment.js
+
+``
+  npm install moment --save
+``
+- Bootstrap (included)
+- Firebase Auth (included)
+
+**Before using this template, please update the .env file with you firbase info if you plan on using firebase for your project; otherwise, delete all firbase boilerplate related code to remove errors.**
--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import firebase from 'firebase';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import './styles/index.scss';
-import App from './App';
+import App from './App/App';
 import reportWebVitals from './reportWebVitals';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
+import firebaseConfig from './helpers/apiKeys';
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.Fragment>
-    <App />
+    <Router>
+      <CssBaseline />
+      <App />
+    </Router>
   </React.Fragment>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
